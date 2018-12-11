daf49325da5a70b29e8e7df10015a60924180916
Fernando Alegría <fernanalegria@gmail.com>
2018-12-12 00:27:36 +0100

    Specify contact model

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,24 +1,32 @@
 import React, { Component } from "react";
-import ContactDetails from './ContactDetails';
-import ContactAvatar from './ContactAvatar';
-import RemoveButton from './RemoveButton';
-import PropTypes from 'prop-types';
+import ContactDetails from "./ContactDetails";
+import ContactAvatar from "./ContactAvatar";
+import RemoveButton from "./RemoveButton";
+import PropTypes from "prop-types";
 
 class ContactCard extends Component {
   render() {
-    const {contact, onDeleteContact} = this.props;
+    const { contact, onDeleteContact } = this.props;
     return (
       <li className="contact-list-item">
         <ContactAvatar url={contact.avatarURL} />
         <ContactDetails name={contact.name} handle={contact.handle} />
-        <RemoveButton id={contact.id} onDeleteContact={onDeleteContact}/>
+        <RemoveButton id={contact.id} onDeleteContact={onDeleteContact} />
       </li>
     );
   }
 }
 
 ContactCard.propTypes = {
-  contact: PropTypes.object.isRequired,
+  contact: PropTypes.shape({
+    id: PropTypes.oneOfType([
+      PropTypes.string.isRequired,
+      PropTypes.number.isRequired
+    ]),
+    name: PropTypes.string.isRequired,
+    handle: PropTypes.string.isRequired,
+    avatarURL: PropTypes.string
+  }).isRequired,
   onDeleteContact: PropTypes.func.isRequired
 };
 
